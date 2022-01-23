5f03b0d86ad0c202f82e55a5e7a732da2e32d814
Bhavyaprnk <91519462+Bhavyaprnk@users.noreply.github.com>
2022-01-23 18:12:33 +0530

    Add files via upload

---
+++ Java Activity/Employee/src/Employee.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+
+/**
+ * @author welcome
+ *
+ */
+ public class Employee
+	static void in()
+	{
+		class Employee
+		{
+			float salary=20000;
+		}
+		class Programmer extends Employee
+		{
+			
+		int bonus=2000;
+		
+		public static void main(String arg[])
+		{
+			Programmer a=new Programmer();
+			System.out.println("Salary is:"+a.salary);
+			System.out.println("Bonus is:"+a.bonus);
+			}
+		}}}
+	
---
+++ Java Activity/St details/src/Example.java
@@ -0,0 +1,14 @@
+
+public class Example {
+	String name;
+	int roll_no;
+}
+class details{
+	public static void main(String[] args) {
+	Example s = new Example();
+	s.name="John";
+	s.roll_no=2;
+    System.out.println("Name is "+s.name+" and roll number is" +s.roll_no);
+	}
+
+}
